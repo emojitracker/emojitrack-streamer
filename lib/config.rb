fff77e3f69e03da3f58e61951e5f285bd4245c82
Matthew Rothenberg <mrothenberg@gmail.com>
2014-04-01 15:19:19 -0400

    fix verbose mode

--- lib/config.rb
+++ lib/config.rb
@@ -1,8 +1,14 @@
 require 'redis'
 require 'uri'
 
+#convenience method for reading booleans from env vars
+def to_boolean(s)
+  s and !!s.match(/^(true|t|yes|y|1)$/i)
+end
+
 # verbose mode or no
-VERBOSE = ENV["VERBOSE"] || false
+VERBOSE = to_boolean(ENV["VERBOSE"]) || false
+puts "*** Starting in VERBOSE mode" if VERBOSE
 
 # db setup
 REDIS_URI = URI.parse(ENV["REDIS_URL"] || ENV["BOXEN_REDIS_URL"] || "redis://localhost:6379")
@@ -32,9 +38,4 @@ def graphite_dyno_log(metric,count)
   dyno = ENV['DYNO'] || 'unknown-host'
   metric_name = "#{dyno}.#{metric}"
   graphite_log metric_name, count
-end
-
-#convenience method for reading booleans from env vars
-def to_boolean(s)
-  s and !!s.match(/^(true|t|yes|y|1)$/i)
 end
