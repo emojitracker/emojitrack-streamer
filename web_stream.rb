828746bad5ece49f737da3c0ee1816088da6356f
Matthew Rothenberg <mrothenberg@gmail.com>
2014-04-22 12:07:34 -0400

    move admin into own file

--- web_stream.rb
+++ web_stream.rb
@@ -3,6 +3,7 @@ require 'oj'
 require 'eventmachine'
 require_relative 'lib/config'
 require_relative 'lib/wrapped_stream'
+require_relative 'lib/web_streamer_admin'
 
 ##############################################################
 # configure defaults around forced SSE timeouts and the like
@@ -219,58 +220,6 @@ class WebKioskInteractionStreamer < Sinatra::Base
 
 end
 
-################################################
-# admin stuff
-################################################
-
-class WebStreamerReporting < Sinatra::Base
-
-  module ReportingUtils
-    STREAM_STATUS_REDIS_KEY = 'admin_stream_status'
-    STREAM_STATUS_UPDATE_RATE = 5
-
-    def self.current_status
-      {
-        'node'        => self.node_name,
-        'status'      => 'OK',
-        'reported_at' => Time.now.to_i,
-        'connections' => {
-          'stream_raw'    => WebScoreRawStreamer.connections.map(&:to_hash),
-          'stream_eps'    => WebScoreCachedStreamer.connections.map(&:to_hash),
-          'stream_detail' => WebDetailStreamer.connections.map(&:to_hash)
-        }
-      }
-    end
-
-    def self.node_name
-      @node_name ||= 'mri-' + ENV['RACK_ENV'] + '-' + (ENV['DYNO'] || 'unknown')
-    end
-
-    def self.push_node_status_to_redis
-      REDIS.HSET STREAM_STATUS_REDIS_KEY, self.node_name, Oj.dump(self.current_status)
-    end
-
-  end
-
-  # periodically log the updates
-  EM.next_tick do
-    EM.add_periodic_timer(ReportingUtils::STREAM_STATUS_UPDATE_RATE) do
-      ReportingUtils.push_node_status_to_redis
-    end
-  end
-
-  helpers ReportingUtils
-
-  get '/admin/?' do
-    redirect '/admin/', 301
-  end
-
-  get '/admin/node.json' do
-    content_type :json
-    Oj.dump ReportingUtils.current_status
-  end
-
-end
 
 ################################################
 # main master class for the app
@@ -281,7 +230,7 @@ class WebStreamer < Sinatra::Base
   use WebScoreRawStreamer         if ENABLE_RAW_STREAM
   use WebScoreCachedStreamer
   use WebDetailStreamer
-  use WebStreamerReporting
+  use WebStreamerAdmin
 
 
   ################################################
@@ -316,16 +265,6 @@ class WebStreamer < Sinatra::Base
     matched_conns.each(&:close)
     content_type :json
     Oj.dump( { 'status' => 'OK', 'closed' => matched_conns.count } )
-  end
-
-  @stream_graphite_log_rate = 10
-  EM.next_tick do
-    # graphite logging for all the streams
-    EM::PeriodicTimer.new(@stream_graphite_log_rate) do
-      graphite_dyno_log("stream.raw.clients",     WebScoreRawStreamer.connections.count)
-      graphite_dyno_log("stream.eps.clients",     WebScoreCachedStreamer.connections.count)
-      graphite_dyno_log("stream.detail.clients",  WebDetailStreamer.connections.count)
-    end
   end
 
 end
